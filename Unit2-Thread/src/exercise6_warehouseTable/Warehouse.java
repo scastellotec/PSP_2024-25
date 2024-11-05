5204f1794bbfa17157ff765f7f5f84db5c85a7dd
SergioCastellote <scastellotec@iespabloserrano.com>
2024-11-05 10:48:56 +0100

    warehouse tabletop and legs

--- Unit2-Thread/src/exercise6_warehouseTable/Warehouse.java
+++ Unit2-Thread/src/exercise6_warehouseTable/Warehouse.java
@@ -10,20 +10,36 @@ public class Warehouse {
     public Warehouse() {
     }
 
-    public void storeLeg(){
+    public synchronized void storeLeg() throws InterruptedException {
+        while(numberLegsStored >= MAX_LEGS){
+            System.out.println("Max legs reached");
+            wait();
+        }
         numberLegsStored++;
+        System.out.println("New leg stored, total: "+numberLegsStored);
+        notifyAll();
 
     }
 
-    public void storeTabletop(){
+    public synchronized void storeTabletop() throws InterruptedException {
+        while(numberTabletopStored >= MAX_TABLETOPS){
+            System.out.println("Max tabletop reached");
+            wait();
+        }
         numberTabletopStored++;
+        System.out.println("New tabletop, total: "+numberTabletopStored);
+        notifyAll();
     }
 
-    public void buildTable(){
+    public synchronized void buildTable() throws InterruptedException {
+        while(numberLegsStored < 4 || numberTabletopStored < 1){
+            System.out.println("Not enough resources");
+            wait();
+        }
         System.out.println("table built!");
         numberTabletopStored -= 1;
         numberLegsStored -= 4;
-
+        notifyAll();
     }
 
 }
